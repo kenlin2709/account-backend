6d42b83b018572b8ed125c662b7e8b437616b99e
kenlin2709 <lin875223761@gmail.com>
2024-04-25 21:14:42 +1000

    UPDATE GET ALL PRODUCT

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -9,6 +9,6 @@ export class ProductService {
   
     async getAllProducts(){
       const allProducts = await this.productModel.find();
-      return allProducts.reduce((obj, item) => (obj[item.itemCode] = item, obj) ,{});
+      return allProducts;
     }
 }
