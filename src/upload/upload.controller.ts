940523f639a9fdd885471baadf51d1c6ce80aeb1
kenlin2709 <lin875223761@gmail.com>
2024-05-24 23:19:39 +1000

    upload file with itemCode

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,24 +1,31 @@
-import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
-    constructor(private readonly uploadService: UploadService){
-
-    }
-    @Post()
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(@UploadedFile(
-        new ParseFilePipe({
-            validators: [
-                new MaxFileSizeValidator({maxSize: 10000000}),
-                // new FileTypeValidator({fileType: 'image/jpeg'}),
-            ]
-        })
-    ) file: Express.Multer.File ) {
-        const filePath = await this.uploadService.upload(file.originalname, file.buffer);
-        return {filePath};
-    }
-    
+  constructor(private readonly uploadService: UploadService) {}
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 10000000 }),
+          // new FileTypeValidator({fileType: 'image/jpeg'}),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() body,
+  ) {
+    console.log(file);
+    const filePath = await this.uploadService.upload(
+      file.originalname,
+      file.buffer,
+      file.mimetype,
+      body.itemCode,
+    );
+    return { filePath };
+  }
 }
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,8 +1,9 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+var mime = require('mime-types')
 export const S3_IMAGE_LINK_PREFIX = 'https://aurifyproduct.s3.ap-southeast-2.amazonaws.com/product-images/'
+
 
 @Injectable()
 export class UploadService {
@@ -11,15 +12,15 @@ export class UploadService {
   });
   constructor(private readonly configService: ConfigService) {}
 
-  async upload(fileName: string, file: Buffer): Promise<string> {
+  async upload(fileName: string, file: Buffer, mimetype:string, itemCode?: string,): Promise<string> {
     await this.s3Client.send(
         new PutObjectCommand({
             Bucket: 'aurifyproduct',
             ACL: 'public-read',
-            Key: 'product-images/' + fileName,
+            Key: 'product-images/' + (itemCode? itemCode + '.' + mime.extension(mimetype): fileName),
             Body: file,
         })
     )
-    return S3_IMAGE_LINK_PREFIX + fileName;
+    return S3_IMAGE_LINK_PREFIX + (itemCode? itemCode + '.' + mime.extension(mimetype): fileName);
   }
 }
